5cca30bc03c25f25184519a1e294b0b7179a27c8
Ethan Knop <ethanknop@Ethans-Mac-mini.local>
2025-11-14 16:52:03 -0600

    Change backend url

--- src/app/services/settings.service.ts
+++ src/app/services/settings.service.ts
@@ -8,10 +8,10 @@ import { Machine } from 'src/models/machine';
 export class SettingsService {
 
   darkMode: WritableSignal<boolean> = signal(true);
-  url: string | null = 'http://g08oggko08okwo0gkckos4kg.24.217.123.15.sslip.io';
+  url: string | null = null;
 
   constructor(private http: HttpClient) {
-    // this.url = this.extractIpAddress(window.location.href);
+    this.url = this.extractIpAddress(window.location.href);
     this.applySettingsFromCookie();
   }
 
